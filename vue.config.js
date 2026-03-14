111f00bf3a7a7cff89779bc52c701c8022d29e0c
mine-cuber <minecuber001@gmail.com>
2026-03-14 16:53:38 -0400

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,3 @@
-/**
- * @type {import('@vue/cli-service').ProjectOptions}
- */
-
 const DEV = process.env.VUE_APP_DEV === "true";
 const STEAM = process.env.VUE_APP_STEAM === "true";
 
@@ -11,5 +7,12 @@ module.exports = {
   outputDir: STEAM ? "../AppFiles" : "dist",
   configureWebpack: {
     devtool: DEV ? "eval-source-map" : "source-map",
-  }
+  },
+  pages: {
+    index: {
+      entry: "src/main.js",       
+      template: "public/index-template.html", // renamed to avoid vue creating a new index.html
+      filename: "index.html", // output file, must be named index.html for the game to work
+    },
+  },
 };
